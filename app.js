eb61c8f3880f9824fc82d72162c3f95c8bb84659
ThaisMK <thais.konishi@gmail.com>
2025-02-23 15:16:53 -0300

    implementação da resolução do challenge conforme requisitos no readme

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+function adicionarAmigo(){
+    let nomeEntrada = document.getElementById("amigo");
+    let nomeAmigo = nomeEntrada.value.trim();
+    nomeEntrada.value = "";
+    if(nomeAmigo !==""){
+        listaAmigos.push(nomeAmigo);
+        exibirLista();
+    }
+else {
+    alert("Por favor, insira um nome válido.");
+    }
+}
+
+function exibirLista(){
+    let campo = document.getElementById("listaAmigos");
+    campo.innerHTML = listaAmigos;
+}
+
+function sortearAmigo(){
+    if (listaAmigos.length === 0) {
+        alert("Adicione amigos antes de sortear.");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSorteado = listaAmigos[indiceSorteado];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = amigoSorteado;
+}
